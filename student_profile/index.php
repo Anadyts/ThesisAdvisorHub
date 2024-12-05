045361b67f43ea521a5c8276150af1fc34c6cd45
Anadyst <potter17180@gmail.com>
2024-12-05 23:00:29 +0700

    add profile student and advisor page

---
+++ student_profile/index.php
@@ -0,0 +1,60 @@
+<?php
+    session_start();
+    if(isset($_POST['logout'])){
+        session_destroy();
+        header('location: /ThesisAdvisorHub/login');
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Advisor List</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+</head>
+<body>
+    <nav>
+        <div class="logo">
+            <img src="../Logo.jpg" alt="" width="90px">
+        </div>
+        <ul>
+            <li><a href="/ThesisAdvisorHub/home">Home</a></li>
+            
+            <?php
+                if(empty($_SESSION['username'])){
+                    echo "
+                        <li><a href='/ThesisAdvisorHub/login'>Login</a></li>
+                        <li><a href='/ThesisAdvisorHub/register'>Register</a></li>
+                    ";
+                }else{
+                    echo "
+                        <li><a href='/ThesisAdvisorHub/advisor'>Advisor</a></li>
+                    ";
+                }
+            ?>
+        </ul>
+
+        <div class="userProfile">
+            <?php
+                if(isset($_SESSION['username'])){
+                    echo '<h2>'.$_SESSION['username'].'<h2/>';
+                    echo "<i class='bx bxs-user-circle' ></i>";
+                    echo "<div class='dropdown'>
+                            <form action='' method='post'>
+                                <button name='profile'>Profile</button>
+                                <button name='logout'>Logout</button>
+                            </form>
+                        </div>";
+                }
+            ?>
+        </div>
+    </nav>
+    
+</body>
+</html>
